718505c1543f26f06811221dc4353a5ad4adda59
maksborze <maxyymussb12@gmail.com>
2025-11-07 13:10:10 +0100

    Using namespace std and Setlocale

--- nure_programming3/nure_programming3.cpp
+++ nure_programming3/nure_programming3.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
-// Українська мова
-#ifdef _WIN32
-#include <windows.h>
-#endif
 
 int main()
 {
-    // Українська мова 
-#ifdef _WIN32
-    SetConsoleCP(1251);          // Ввід з консолі
-    SetConsoleOutputCP(1251);    // Вивід у консоль
-#endif
+    setlocale(LC_ALL, "Ukrainian");
+
 
  
     int apartment;
 
-    std::cout << "Введіть номер квартири: ";
-    if (!(std::cin >> apartment)) {
-        std::cout << "Помилка! Ви ввели не номер квартири .\n";
+    cout << "Введіть номер квартири: ";
+    if (!(cin >> apartment)) {
+        cout << "Помилка! Ви ввели не номер квартири .\n";
         return 1;
     }
 
@@ -32,18 +25,18 @@ int main()
     case 3:  owner = "Гольдінер";   break;
     case 4:  owner = "Ковальчук";   break;
     case 5:  owner = "Шевченко";    break;
-    case 6:  owner = "Савченко";       break;
+    case 6:  owner = "Савченко";    break;
     case 7:  owner = "Мельник";     break;
-    case 8:  owner = "Зоря";    break;
+    case 8:  owner = "Зоря";        break;
     case 9:  owner = "Борзенков";   break;
     case 10: owner = "Олійник";     break;
     default:
-        std::cout << "Результат: Квартири з таким номером в домі немає!" << std::endl;
+        cout << "Результат: Квартири з таким номером в домі немає!" << endl;
         return 0;
     }
 
     // Результат
-    std:cout << "Результат: Власник квартири №" << apartment << " – " << owner << std::endl;
+    cout << "Результат: Власник квартири №" << apartment << " – " << owner << endl;
 
     return 0;
 }
